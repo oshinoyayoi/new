5be7879bbbdf967675a375383f9bfdabf8b3c1da
oshinoyayoi <oshinoyayoi@gmail.com>
2022-07-21 16:30:01 +0900

    learn

--- category/src/components/MenuItems.jsx
+++ category/src/components/MenuItems.jsx
@@ -2,16 +2,21 @@ import SubMenu from "./subMenu.components";
 import "./MenuItems.styles.css";
 
 const MenuItem = ({ Item }) => {
-  const { categoryName, subList } = Item;
+  const { categoryName, subList, categoryImage } = Item;
   return (
     <div className="first-level-categories">
       <div className="first-category-head">{categoryName}</div>
       <div className="second-background">
-        <ul className="category-List">
-          {subList.map((sub) => {
-            return <SubMenu key={sub.categoryId} sub={sub} />;
-          })}
-        </ul>
+        <div className="img">
+          <img src={categoryImage} alt="imgs" />
+
+          <p className="category-name">{categoryName}</p>
+          <ul className="category-List">
+            {subList.map((sub) => {
+              return <SubMenu key={sub.categoryId} sub={sub} />;
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
